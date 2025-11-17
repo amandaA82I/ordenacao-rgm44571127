8e9b874f8974916554ba7f8de2723065d16506c2
amandaA82I <afreitasrosencrantzaiello@gmail.com>
2025-11-17 09:30:49 -0300

    Create sorts.h

---
+++ sorts.h
@@ -0,0 +1,19 @@
+COLE este arquivo)
+
+#ifndef SORTS_H
+#define SORTS_H
+
+#include <stddef.h>
+
+typedef struct {
+    long long cmp;
+    long long swp;
+} Metrics;
+
+void reset_metrics(Metrics *m);
+
+void insertion_sort(int *v, size_t n, Metrics *m);
+void merge_sort(int *v, size_t n, Metrics *m);
+void quick_sort(int *v, size_t n, Metrics *m);
+
+#endif
